e9d7fed0323ce18973033a2561b9bb1376107bbb
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2026-01-08 15:47:43 +0200

    correcting task 2

--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/public'
+    path: path.resolve(__dirname, 'public')
   },
   mode: 'production'
 };
